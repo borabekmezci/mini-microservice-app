f8ce1237ea7d35dcdd30abd3e00c0af5d9406688
borabekmezci <borabekmezci@gmail.com>
2021-11-10 18:04:56 +0300

    added index and modified package.json

---
+++ comments/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+
+const app = express ();
+app.use(express.json());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments",(req,res)=>{
+    res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments",(req,res)=>{
+    const commentId = randomBytes(4).toString("hex");//generated new id for comment
+    const { content } = req.body;//created content for comment(which can mean that we write comment via post request)
+
+    const comments = commentsByPostId[req.params.id] || [];//we took id number in the url as paramater
+
+    comments.push({id : commentId , content});//we push our id(parameter in post request) and content to comments
+    commentsByPostId[req.params.id] = comments;// comments we created is known via its id.commentsByPostId[req.params.id]
+    res.status(201).send(comments);
+});
+
+app.listen(4001,()=>{
+    console.log("Listening on 4001!");
+});
