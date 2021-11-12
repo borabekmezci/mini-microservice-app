17c644b5310a2729eb7da013e48e43ab7e1c6163
borabekmezci <borabekmezci@gmail.com>
2021-11-12 15:50:25 +0300

    created Postlist,CommentCreate and CommentList

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React, { useState , useEffect} from "react";
 import axios from "axios";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 const PostList = () => {
     const [posts,setPosts] = useState({});
@@ -15,16 +17,20 @@ const PostList = () => {
         fetchPosts();
     },[]);
 
-    const renderedPosts = Object.values (posts).map(post =>{
-        return <div 
+    const renderedPosts = Object.values(posts).map(post =>{
+        return (
+        <div 
         className="card" 
         style ={{width : "30%" , marginBottom : "20px"}}
         key = {post.id}
         >
             <div className = "card-body">
                 <h3>{post.title}</h3>
-            </div>
+                <CommentList postId = {post.id} />
+                <CommentCreate postId = {post.id} />
+            </div>            
         </div>
+        );
     }) ;
 
     return <div className = "d-flex flex-row flex-wrap justify-content-between">
