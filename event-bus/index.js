0067bc1f22769ef38b56dc6f46a624770e0e55a2
borabekmezci <borabekmezci.com>
2022-07-25 11:59:37 +0300

    Event bus modification

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,40 +7,33 @@ app.use(express.json());
 
 const events = [];
 
-app.post("/events",(req,res) => {
-    const event =req.body;
-    
-    events.push(event) ;
+app.post("/events", (req, res) => {
+    const event = req.body;
 
-    axios.post('http://localhost:4000/events',event).catch((err)=>{
-        console.log(err.message);
-       
-    });
-    axios.post('http://localhost:4001/events',event).catch((err) => {
-        console.log(err.message);
-    });
-    axios.post('http://localhost:4002/events',event).catch((err) => {
-        console.log(err.message);
-    });
-    axios.post('http://localhost:4003/events',event).catch((err)=>{
-        console.log(err.message);
-    });
-    axios.post('http://localhost:4006/events',event).catch((err)=>{
-        console.log(err.message);
-    });
-    axios.post('http://localhost:4007/events',event).catch((err)=>{
-        console.log(err.message);
-    });
+    events.push(event);
 
+    axios.post('http://posts-clusterip-srv:4000/events', event).catch((err) => {
+        console.log(err.message);
 
-    res.send({status : "OK"});
-    
+    });
+    /*   axios.post('http://localhost:4001/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('http://localhost:4002/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('http://localhost:4003/events', event).catch((err) => {
+        console.log(err.message);
+    });
+*/
+    res.send({ status: "OK" });
+
 });
 
-app.get('/events',(req,res)=>{
+app.get('/events', (req, res) => {
     res.send(events);
 });
 
-app.listen(4005 , () => {
+app.listen(4005, () => {
     console.log('Listening on 4005!');
 });
